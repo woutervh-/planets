4efe25b482868ae4eed70f5a0ba5eaf93be19167
Wouter van Heeswijk <woutervh@gmail.com>
2020-11-14 20:43:50 +0100

    Refactor task

--- Assets/Scripts/SQT/Displacement/NoneDisplacement.cs
+++ Assets/Scripts/SQT/Displacement/NoneDisplacement.cs
@@ -6,8 +6,9 @@ namespace SQT.Displacement
 {
     public class NoneDisplacement : MeshModifier
     {
-        public async Task ModifyVertices(SQT.Context context, SQT.Node node, CancellationTokenSource cancellation)
+        public Task ModifyVertices(SQT.Context context, SQT.Node node, CancellationTokenSource cancellation)
         {
+            return Task.FromResult<object>(null);
         }
 
         public void ModifyMaterial(Material material)
